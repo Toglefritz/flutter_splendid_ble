1265ef6e1bba19d7dad5834b145863a1e0be8099
Scott Hatfield <contact@hatfieldscott.com>
2023-10-07 08:00:50 -0500

    Added test for emitCurrentBluetoothStatus method :pretzel:

---
+++ test/emit_current_bluetooth_status_test.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_ble/models/bluetooth_status.dart';
+import 'package:flutter_ble/src/channel/flutter_ble_method_channel.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  const MethodChannel channel = MethodChannel('flutter_ble');
+
+  setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel,
+        (MethodCall methodCall) async {
+      if (methodCall.method == 'emitCurrentBluetoothStatus') {
+        return null;
+      }
+      return null;
+    });
+  });
+
+  test('emitCurrentBluetoothStatus emits correct BluetoothStatus', () async {
+    final MethodChannelFlutterBle methodChannelFlutterBle = MethodChannelFlutterBle();
+
+    // Create a list to hold emitted statuses
+    final List<BluetoothStatus> emittedStatuses = <BluetoothStatus>[];
+
+    final Stream<BluetoothStatus> statusStream = methodChannelFlutterBle.emitCurrentBluetoothStatus();
+
+    // Listen to the stream and add emitted statuses to the list
+    final StreamSubscription<BluetoothStatus> subscription = statusStream.listen((status) {
+      emittedStatuses.add(status);
+    });
+
+    // Trigger the platform side to emit BluetoothStatus.enabled
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
+      channel.name,
+      const StandardMethodCodec().encodeMethodCall(const MethodCall('adapterStateUpdated', 'enabled')),
+      (ByteData? data) {},
+    );
+
+    // After a delay, change the emitted status to BluetoothStatus.disabled
+    Future.delayed(const Duration(milliseconds: 100), () {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
+        channel.name,
+        const StandardMethodCodec().encodeMethodCall(const MethodCall('adapterStateUpdated', 'disabled')),
+        (ByteData? data) {},
+      );
+    });
+
+    // After a delay, verify the emitted statuses
+    await Future.delayed(const Duration(milliseconds: 200), () {
+      expect(emittedStatuses, [BluetoothStatus.enabled, BluetoothStatus.disabled]);
+      subscription.cancel();
+    });
+  });
+
+  tearDown(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+      channel,
+      null,
+    );
+  });
+}
