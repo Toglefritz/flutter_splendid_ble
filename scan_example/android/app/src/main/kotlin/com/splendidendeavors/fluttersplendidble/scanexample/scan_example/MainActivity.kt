d902438425f9c6a1a30b0dc560e3a0e12b3c3a46
Scott Hatfield <contact@hatfieldscott.com>
2023-11-17 11:17:00 -0600

    Removed extra examples :wave:

--- scan_example/android/app/src/main/kotlin/com/splendidendeavors/fluttersplendidble/scanexample/scan_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.splendidendeavors.fluttersplendidble.scanexample.scan_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- scan_example/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:scan_example/splendid_ble_scan_example_app.dart';
-
-void main() {
-  runApp(const SplendidBleScanExampleApp());
-}
