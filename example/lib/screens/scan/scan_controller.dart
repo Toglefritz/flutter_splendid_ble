ef34130747f9d80f5513905e67e61d3327c04088
Scott Hatfield <contact@hatfieldscott.com>
2024-01-15 07:03:34 -0600

    Clear results when restarting scan :satellite:

--- example/lib/screens/scan/scan_controller.dart
+++ example/lib/screens/scan/scan_controller.dart
@@ -40,11 +40,13 @@ class ScanController extends State<ScanRoute> {
   /// The [startScan] stream delivers an instance of [BleDevice] to the callback which contains information about
   /// the Bluetooth device.
   ///
-  /// The
+  /// Various filters can be applied to the scanning process to limit the selection of devices returned by the scan.
+  /// See the [ScanFilter] class for full information about the available filters. But the most common filtering
+  /// option is typically filtering by the UUID of the primary service of the BLE devices detected by the scan. This
+  /// allows manufacturers of Bluetooth devices to ensure that only their devices are returned by the Bluetooth scan,
+  /// which is obviously useful for building a companion mobile app for these devices.
   void _startBluetoothScan() {
-    _scanStream = _ble
-        .startScan(filters: widget.filters, settings: widget.settings)
-        .listen(
+    _scanStream = _ble.startScan(filters: widget.filters, settings: widget.settings).listen(
       (device) => _onDeviceDetected(device),
       onError: (error) {
         // Handle the error here
@@ -80,9 +82,7 @@ class ScanController extends State<ScanRoute> {
     debugPrint('Discovered BLE device: ${device.name}');
 
     // Add the newly discovered device to the list only if it not already in the list
-    if (discoveredDevices
-        .where((discoveredDevice) => discoveredDevice.address == device.address)
-        .isEmpty) {
+    if (discoveredDevices.where((discoveredDevice) => discoveredDevice.address == device.address).isEmpty) {
       setState(() {
         discoveredDevices.add(device);
       });
@@ -116,6 +116,11 @@ class ScanController extends State<ScanRoute> {
         _scanInProgress = false;
       });
     } else {
+      // Clear the existing discovered devices
+      setState(() {
+        discoveredDevices.clear();
+      });
+
       _startBluetoothScan();
     }
   }
