836516563e9347c5640f73d3c63efc01b6a54e54
Scott Hatfield <contact@hatfieldscott.com>
2026-07-23 21:24:50 -0500

    Added support for SPM

---
+++ ios/flutter_splendid_ble/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "flutter_splendid_ble",
+    platforms: [
+        .iOS("13.0")
+    ],
+    products: [
+        .library(name: "flutter-splendid-ble", targets: ["flutter_splendid_ble"])
+    ],
+    dependencies: [
+        .package(name: "FlutterFramework", path: "../FlutterFramework")
+    ],
+    targets: [
+        .target(
+            name: "flutter_splendid_ble",
+            dependencies: [
+                .product(name: "FlutterFramework", package: "FlutterFramework")
+            ]
+        )
+    ]
+)
